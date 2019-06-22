b4cf807c586d839e06f3c477fd0d0569d9c38c81
Ray Zane <raymondzane@gmail.com>
2019-06-22 16:35:58 -0400

    Test executing with context

--- spec/graphql/extras/rspec_spec.rb
+++ spec/graphql/extras/rspec_spec.rb
@@ -16,12 +16,22 @@ RSpec.describe GraphQL::Extras::RSpec, type: :graphql do
   end
 
   describe "#use_schema" do
+    let(:context) { { name: "Ray" } }
+    let(:schema)  { use_schema(Schema, context: context) }
+    let(:queries) { graphql_fixture("hello.graphql") }
+
     it "allows easily executing queries" do
-      schema = use_schema(Schema)
-      queries = graphql_fixture("hello.graphql")
       result = schema.execute(queries.hello)
+
       expect(result).to be_successful_query
       expect(result[:data][:hello]).to eq("world")
+    end
+
+    it "allows executing queries with context" do
+      result = schema.execute(queries.hello_context)
+
+      expect(result).to be_successful_query
+      expect(result[:data][:hello]).to eq("Ray")
     end
   end
 end
